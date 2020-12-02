034bb5e52bcddeb64bf9d0944fb241abcb58faa3
seoyeon kim <westkitedev@gmail.com>
2020-12-02 16:24:08 +0900

    Fix : window 환경에서 한글 깨짐 오류 수정

--- back/src/lib/python/naverRealtime.py
+++ back/src/lib/python/naverRealtime.py
@@ -1,14 +1,16 @@
+# -*- coding: utf-8 -*- 
 import requests
 import sys
+import json
 from datetime import datetime
 def getNaverRealtimeSearchWord(urlParam):
     urlParam = {
         'age': 'all',
         'datetime': datetime.now().strftime('%Y-%m-%dT%H:%M:00'),
-        'entertainment':0,
-        'groupingLevel': 0,
+        'entertainment':4,
+        'groupingLevel': 4,
         'marketing': 0,
-        'news': 0,
+        'news': 4,
         'sports': 0,
     }
     url = 'https://m.datalab.naver.com/rankSnapshotV3/data.naver?'
@@ -19,7 +21,7 @@ def getNaverRealtimeSearchWord(urlParam):
     headers = {
         'host': 'm.datalab.naver.com:443',
         'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
-        'accept-encoding': 'gzip, deflate, br',
+        'accept-encoding': 'gzip, deflate, br, charset=UTF-8',
         'accept-language': 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7',
         'sec-fetch-mode': 'nested-navigate',
         'sec-fetch-site': 'same-origin',
@@ -42,5 +44,6 @@ if __name__ == "__main__":
         # 'sports': 2,
     }
     response = getNaverRealtimeSearchWord(param)
-    print(response)
+    jsonString = json.dumps(response)
+    print(jsonString)
     sys.stdout.flush()
